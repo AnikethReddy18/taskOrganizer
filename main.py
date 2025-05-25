7c0b5f1789f807383deed4ff54f963edffe182bc
aniketh <sunnyrockzzz1811@gmail.com>
2025-05-25 13:25:39 +0530

    Add Select File

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
-from PyQt6.QtWidgets import QApplication, QWidget
+from PyQt6.QtWidgets import QApplication, QMainWindow, QFileDialog
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Task Organizer")
+        self.open_file()
+
+    def open_file(self):
+        file_dialog = QFileDialog()
+        file_path, _ = file_dialog.getOpenFileName(self, "Open Excel file", "", "Excel Files (*.xlsx)")
+
+
 
 app = QApplication([])
-
-window = QWidget()
+window = MainWindow()
 window.show()
-
 app.exec()
