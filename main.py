fcd0f79a87bffd9b53c7c89591017628ca5e0203
aniketh <sunnyrockzzz1811@gmail.com>
2025-05-25 16:14:20 +0530

    Implement Done Button

--- main.py
+++ main.py
@@ -16,40 +16,48 @@ class MainWindow(QMainWindow):
 
     def open_file(self):
         #file_dialog = QFileDialog()
-        #file_path, _ = file_dialog.getOpenFileName(self, "Open Excel file", "", "Excel Files (*.xlsx)")
-        workbook = openpyxl.load_workbook("/home/aniketh/Downloads/DSA.xlsx")
-        sheet = workbook.active
+        #self.file_path, _ = file_dialog.getOpenFileName(self, "Open Excel file", "", "Excel Files (*.xlsx)")
+        self.file_path = "/home/aniketh/Downloads/DSA.xlsx"
+        self.workbook = openpyxl.load_workbook(self.file_path)
+        sheet = self.workbook.active
  
-        if "done" in workbook.sheetnames:
+        if "done" in self.workbook.sheetnames:
             pass
         else:
-            workbook.create_sheet('done')
-            workbook.save("/home/aniketh/Downloads/DSA.xlsx")
+            self.workbook.create_sheet('done')
         return sheet
 
     def get_data(self, sheet):
         data = []
 
+        self.done_status = []
         for row in sheet:
             if row[0].value == None:
                 break
             title = row[0].value
             description = row[1].value
             link = row[1].hyperlink.target if row[1].hyperlink else None
+            self.done_status.append(1 if row[2].value == "Done" else 0)
 
             data.append([title, description, link])
-        
         return data
-
+    
     def display_data(self, data):
         layout = QGridLayout()
 
         for row_index,row in enumerate(data):
+            if(self.done_status[row_index] == 1):
+                continue
             for column_index,cell in enumerate(row):
                 label = QLabel(cell)
                 label.setWordWrap(True)
                 layout.addWidget(label, row_index, column_index)
-            layout.addWidget(QPushButton("Done"), row_index, len(row))
+            button = QPushButton("Done")
+            button.clicked.connect(self.done_button_was_pressed)
+            button.setProperty("index", row_index)
+            self.columns = len(row)
+            
+            layout.addWidget(button, row_index, self.columns)
 
         widget = QWidget()
         widget.setLayout(layout)
@@ -57,7 +65,16 @@ class MainWindow(QMainWindow):
         scroll.setWidget(widget)
         self.setCentralWidget(scroll)
 
-    def 
+    def done_button_was_pressed(self):
+        index = self.sender().property('index')
+        self.workbook.active.cell(row=index+1, column=self.columns).value = "Done"
+        self.workbook.save(self.file_path)
+
+        data = self.get_data(self.workbook.active)
+        self.setCentralWidget(QWidget())
+        self.display_data(data)
+    
+        
 
 app = QApplication([])
 window = MainWindow()
