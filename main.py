25415c05cc55f00ca4bae2f9976874103c3c676c
aniketh <sunnyrockzzz1811@gmail.com>
2025-05-26 22:30:47 +0530

    Error Fix and Code Cleanup with comments

--- main.py
+++ main.py
@@ -7,34 +7,38 @@ import random
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+
+        # Window Management
         self.setWindowTitle("Task Organizer")
         self.resize(820, 400)
 
-        self.undone_rows =[]
+        # Initialize Rows
+        self.rows =[]
         self.done_rows = []
-        sheet = self.open_file()
-        self.get_data(sheet)
-        self.row_widgets = []
 
+        # Get Data
+        self.open_file()
+        self.get_data()
+
+        # Display Data
         self.display_data()
 
     def open_file(self):
-        #file_dialog = QFileDialog()
-        #self.file_path, _ = file_dialog.getOpenFileName(self, "Open Excel file", "", "Excel Files (*.xlsx)")
-        self.file_path = "/home/aniketh/Downloads/DSA.xlsx"
+        # Open Excel File
+        file_dialog = QFileDialog()
+        self.file_path, _ = file_dialog.getOpenFileName(self, "Open Excel file", "", "Excel Files (*.xlsx)")
         self.workbook = openpyxl.load_workbook(self.file_path)
         sheet = self.workbook.active
- 
-        if "done" in self.workbook.sheetnames:
-            pass
-        else:
-            self.workbook.create_sheet('done')
-        return sheet
 
-    def get_data(self, sheet):
+        # Make "Done" Column if does not exist
+        headers = [cell.value for cell in sheet[1]]
+        if "Task Status" not in headers:
+            sheet.cell(row=1, column=3).value = "Task Status"
+            self.workbook.save(self.file_path)
 
-        self.done_status = []
-        for i, row in enumerate(sheet.iter_rows()):
+    def get_data(self):
+        # Make arrays for done rows and undone rows
+        for i, row in enumerate(self.workbook.active.iter_rows()):
             if i == 0:
                 continue
             if row[0].value == None:
@@ -42,24 +46,32 @@ class MainWindow(QMainWindow):
             title = row[0].value
             description = row[1].value
             link = row[1].hyperlink.target if row[1].hyperlink else None
+            self.rows.append([title, description, link])
             if row[2].value == "Done":
-                self.done_rows.append([title, description, link])
+                self.done_rows.append(1)
             else:
-                self.undone_rows.append([title, description, link])        
+                self.done_rows.append(0)      
     
     def display_data(self):
         layout = QGridLayout()
+        self.row_widgets = []
 
+        # Labels for heading row
         for column_index,cell in enumerate(list(self.workbook.active.iter_rows())[0]):
                 label = QLabel(cell.value)
                 label.setWordWrap(True)
                 layout.addWidget(label, 0, column_index)
 
+        # Select Random row button        
         select_random_button = QPushButton("Select Random")
         select_random_button.pressed.connect(self.select_random_button_was_pressed)
         layout.addWidget(select_random_button, 0, 2, 1, 2)    
         
-        for row_index,row in enumerate(self.undone_rows):
+        # Add Undone rows to layout
+        for row_index,row in enumerate(self.rows):
+            # Skip display if task is done
+            if self.done_rows[row_index] == 1:
+                continue
             for column_index,cell in enumerate(row):
                 label = QLabel(cell)
                 label.setWordWrap(True)
@@ -72,6 +84,7 @@ class MainWindow(QMainWindow):
             layout.addWidget(button, row_index+1, self.columns)
             self.row_widgets.append(button)
 
+        # Make layout scrollable
         widget = QWidget()
         widget.setLayout(layout)
         self.scroll = QScrollArea()
@@ -79,18 +92,19 @@ class MainWindow(QMainWindow):
         self.setCentralWidget(self.scroll)
 
     def done_button_was_pressed(self):
+        # Get the index and edit the file to done
         index = self.sender().property('index')
-        self.workbook.active.cell(row=index+1, column=self.columns).value = "Done"
+        self.workbook.active.cell(row=index+2, column=self.columns).value = "Done"
         self.workbook.save(self.file_path)
 
-        removed_row = self.undone_rows.pop(index)
-        self.done_rows.append(removed_row)
+        # Update done_rows
+        self.done_rows[index] = 1
+        # Update display to new data
         self.setCentralWidget(QWidget())
-        self.row_widgets = []
         self.display_data()
     
     def select_random_button_was_pressed(self):
-        selected_row_index = random.randint(0, len(self.undone_rows)-1)
+        selected_row_index = random.randint(0, self.done_rows.count(0)-1)
         elem = self.row_widgets[selected_row_index]
         elem.setStyleSheet("color: red")
         self.scroll.ensureWidgetVisible(elem)
